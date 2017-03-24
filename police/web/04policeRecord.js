17a1761f311d2e3e0592352dc147b731a99db9c2
robert yin <robert yin>
2017-03-24 13:41:10 +0800

    r

--- police/web/04policeRecord.js
+++ police/web/04policeRecord.js
@@ -2,6 +2,10 @@ require("/web/useForm.js");
 require("/web/usePage.js");
 require("web/btnTab.js");
 
+layui.use('laydate', function(){
+	var laydate = layui.laydate
+});
+
 layui.use(['element', 'layer'], function(){
 	var element = layui.element();
 	var layer = layui.layer;
